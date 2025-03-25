7c34c1fcae95c8fcacec365e06b26962c9666597
sasithhansaka <sasithhansaka285@gmail.com>
2025-03-25 15:12:52 +0530

    Add authentication page route and global styles

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import AuthenticationPage from './pages/AuthenticationPage'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <Routes>
       <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/' element={<HomePage/>}></Route>
+      <Route path='/auth' element={<AuthenticationPage/>}></Route>
       <Route path='/' element={<HomePage/>}></Route>
       </Routes>
     </Router>
