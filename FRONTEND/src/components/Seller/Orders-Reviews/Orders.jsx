7c37e50ad221d2df9f94d59d5de72e1b7ebafde8
sasithhansaka <sasithhansaka285@gmail.com>
2025-05-14 23:59:34 +0530

    feat: add Orders component with Breadcrumbs and styled title

--- FRONTEND/src/components/Seller/Orders-Reviews/Orders.jsx
+++ FRONTEND/src/components/Seller/Orders-Reviews/Orders.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Breadcrumbs from "../Breadcrumbs";
+import styles from "./Orders.module.css";
 
 function Orders() {
   return (
     <div>
-      
+        <Breadcrumbs />
+      <h3 className={styles.addProductTitle}>Orders</h3>
     </div>
   )
 }
