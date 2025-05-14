57825b76cc0017991ccb93d9b1ebd1dd0e1a2d54
sasithhansaka <sasithhansaka285@gmail.com>
2025-05-14 23:27:49 +0530

    feat: implement Growth component with Breadcrumbs and styled title

--- FRONTEND/src/components/Seller/Growth/Growth.jsx
+++ FRONTEND/src/components/Seller/Growth/Growth.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Breadcrumbs from "../Breadcrumbs";
+import styles from "./Growth.module.css";
 
 function Growth() {
   return (
     <div>
-      
+      <Breadcrumbs />
+      <h3 className={styles.addProductTitle}>Growth</h3>
     </div>
-  )
+  );
 }
 
-export default Growth
+export default Growth;
